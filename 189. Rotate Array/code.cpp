ea8a1b00046ebd46003bb481edec8a040bd0b9ae
Nayan Das <nayan010das@gmail.com>
2025-04-14 00:54:16 +0530

    189

--- 189. Rotate Array/code.cpp
+++ 189. Rotate Array/code.cpp
@@ -35,3 +35,15 @@ class Solution {
             }
         }
 };
+
+// Method-1
+// ⏳TC- O(n) || 🗃️SC = O(1)
+class Solution {
+    public:
+        void rotate(vector<int>& nums, int k) {
+            k = k % nums.size(); //There may be some cases where k>n
+            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(),nums.begin()+k);
+            reverse(nums.begin()+k,nums.end());
+        }
+    };
